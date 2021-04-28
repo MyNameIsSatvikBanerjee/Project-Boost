98db33b5c3741944e2b9979210809181a6714bbe
MyNameIsSatvikBanerjee <banerjeesatvik@gmail.com>
2021-04-28 15:45:11 +0530

    Initial Commit
    Added the whole game while working.

---
+++ Assets/3_Project_Boost-master/Assets/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 0f;
+
+    [SerializeField] Text timeText;
+
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    void Update()
+    {
+        currentTime += 1 * Time.deltaTime;
+        timeText.text = currentTime.ToString();
+    }
+}
